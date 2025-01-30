367afac523f18eb063791fadfc25d6f6a1481983
jojocherri123 <jojocherri123@gmail.com>
2025-01-30 17:20:56 +0300

    changed timer date, and some minor modifications

--- countDown.js
+++ countDown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 17 , 2025 00:00:0").getTime();
+var countDownDate = new Date("May 9 , 2025 00:00:0").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -9,7 +9,7 @@ var x = setInterval(function() {
     
   // Find the distance between now and the count down date
     var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
